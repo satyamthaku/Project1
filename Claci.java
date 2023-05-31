ac5d1115db216f770772f27f167a7a68f4c1d72a
satyamthaku <satyamthakur880@gmail.com>
2023-05-31 18:28:22 +0530

    changed numbers

--- Claci.java
+++ Claci.java
@@ -3,9 +3,9 @@ package calculator;
 public class Claci {
 
 	public static void main(String[] args) {
-		int a=5;
-		int b=6;
-		int c=a+b;
+		int a = 7;
+		int b = 3;
+		int c = a + b;
 		System.out.println(c);
 
 	}
